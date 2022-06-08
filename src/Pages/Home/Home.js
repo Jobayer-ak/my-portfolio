15f74e08d92a86fe25b3765c45cb7e9d23a724c9
Jobayer_ak <jobayer.ak@gmail.com>
2022-06-09 01:07:40 +0600

    working with home page

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../Shared/Navbar';
+import "./Home.css"
+
+const Home = () => {
+    return (
+        <div className=''>
+            
+        </div>
+    );
+};
+
+export default Home;
