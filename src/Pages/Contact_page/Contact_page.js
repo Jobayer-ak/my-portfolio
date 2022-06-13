046fe2a7074e368114079562822da5f9e79a66bb
Jobayer_ak <jobayer.ak@gmail.com>
2022-06-13 15:22:00 +0600

    working on contact page

--- src/Pages/Contact_page/Contact_page.js
+++ src/Pages/Contact_page/Contact_page.js
@@ -1,11 +1,62 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
+import "./Contact_page.css";
 
 const Contact_page = () => {
-    return (
-        <div>
-            <h2>Contact Page</h2>
-        </div>
-    );
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <div className="contact_page">
+      <div className="container mx-auto px-12 text-center py-5">
+        <h2 className="text-center text-4xl text-primary font-bold my-7">
+          Contact Page
+        </h2>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            type="text"
+            className="input input-bordered w-full max-w-xs"
+            placeholder="Your Name"
+            {...register("example")}
+          />
+
+          <br />
+          <br />
+          <input
+            type="email"
+            className="input input-bordered w-full max-w-xs"
+            placeholder="Email"
+            {...register("exampleRequired", { required: true })}
+          />
+            <br/>
+            <br/>
+          <textarea
+            type="text"
+            className="input input-bordered w-full max-w-xs"
+            placeholder="Your Message"
+            rows={20}
+            cols={20}
+            {...register("exampleRequired", { required: true })}
+          />
+          <br />
+          <br />
+
+          <p className="text-red-700">
+            {errors.exampleRequired && <span>This field is required</span>}
+          </p>
+
+          <input className="btn btn-primary" type="submit" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact_page;
