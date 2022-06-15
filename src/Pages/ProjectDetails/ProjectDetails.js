f8b69ac78baff465ae1759337178dda82dbaa651
Jobayer_ak <jobayer.ak@gmail.com>
2022-06-15 20:49:53 +0600

    slider images and all live links and code links are added

--- src/Pages/ProjectDetails/ProjectDetails.js
+++ src/Pages/ProjectDetails/ProjectDetails.js
@@ -1,21 +1,29 @@
-import React from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import React from "react";
+import { useLocation, useParams } from "react-router-dom";
 
 const ProjectDetails = () => {
-   const location = useLocation();
-    
-    const name = location.state.name;
-    const features = location.state.features;
-    const technology = location.state.technology;
-    
-    return (
-        <div>
-            <h2 className='text-center'>Project Details</h2>
-            <p>{name}</p>
-            <p>{features}</p>
-            <p>{technology}</p>
-        </div>
-    );
+  const location = useLocation();
+
+  const name = location.state.name;
+  const features = location.state.features;
+  const technology = location.state.technology;
+  const slider_1= location.state.slider_1;
+  const slider_2= location.state.slider_2;
+  const slider_3= location.state.slider_3;
+  const slider_4= location.state.slider_4;
+
+
+  return (
+    <div>
+      <div className="container px-12 mx-auto my-5">
+        <h2 className="text-4xl font-bold text-center">Project Details</h2>
+        <p>Project Name: {name}</p>
+        <li>{features}</li>
+        <p>{technology}</p>
+        <img src={slider_1}></img>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectDetails;
